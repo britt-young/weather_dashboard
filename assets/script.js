0c7712d34280f4a5bca3483f94516ea809f23e03
Britt Young <140761880+britt-young@users.noreply.github.com>
2023-10-16 11:41:31 -0400

    styles
    changed date format in js file

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
     // HTML for the main weather card
     if(index === 0) { 
         return `<div class="details">
-                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0].replace(/-/g, '/')})</h2>
                     <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
@@ -23,7 +23,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
     } else { 
         // HTML for the other five day forecast card
         return `<li class="card">
-                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+                    <h3>${cityName} (${weatherItem.dt_txt.split(" ")[0].replace(/-/g, '/')})</h3>
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
                     <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
